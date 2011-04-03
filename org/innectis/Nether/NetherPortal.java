b6618fc3675514ce1b35ab1f10bde77a2aebd105
SpaceManiac <tad.hardesty@suddenlink.net>
2011-04-03 14:25:28 -0500

    More fixes

--- org/innectis/Nether/NetherPortal.java
+++ org/innectis/Nether/NetherPortal.java
@@ -48,10 +48,10 @@ public class NetherPortal {
 	
 	
 	// Check at an arbitrary location, used for failed teleport, to move back out of the portal frame.
-	public static Location getVerifiedSpawn(Block checkBlock){
+	public static Location getVerifiedSpawn(Block checkBlock) {
 		List<Block> list = listSpawns(checkBlock, true, 0);
 		
-		if(list != null && !list.isEmpty()) {
+		if (list != null && !list.isEmpty()) {
 			// Use a random verified location to spawn.
 			Block targetBlock = list.get((int) (Math.random() * list.size()));
 			return new Location(checkBlock.getWorld(), targetBlock.getX() + 0.5, targetBlock.getY() - 0.75, targetBlock.getZ() + 0.5);
@@ -66,30 +66,30 @@ public class NetherPortal {
 	
 	// Lists the valid spawns near a portal, you should pass this method one of the two top portal blocks.
 	// Checks 4 spawns per portal block, for a total of 24.
-	private static List<Block> listSpawns(Block theBlock, boolean iteratePortal, int direction){
+	private static List<Block> listSpawns(Block theBlock, boolean iteratePortal, int direction) {
 		Block checkBlock;
 		final BlockFace[] faces = {BlockFace.DOWN, BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST};
 		List<Block> list = new ArrayList<Block>();
 		
 		// Scan for an air pocket beside portal blocks.
-		for (int x = 0; x < faces.length; ++x){
+		for (int x = 0; x < faces.length; ++x) {
 			checkBlock = theBlock.getRelative(faces[x]);
 			
 			if (checkBlock.getType() == Material.PORTAL) {
-				if(iteratePortal){
+				if (iteratePortal) {
 					// If it is another portal block, check beside it too.  And below: (faces[x] == BlockFace.DOWN)
-					if(x == 0) list.addAll(listSpawns(checkBlock, true, x));
+					if (x == 0) list.addAll(listSpawns(checkBlock, true, x));
 					else list.addAll(listSpawns(checkBlock, false, x));
 				}
 			} else if (checkBlock.getType() != Material.OBSIDIAN) {
 				// Check for air directly beside portal.
-				if (canBreathe(checkBlock.getTypeId()) && canBreathe(checkBlock.getRelative(BlockFace.DOWN).getTypeId())){
+				if (canBreathe(checkBlock.getTypeId()) && canBreathe(checkBlock.getRelative(BlockFace.DOWN).getTypeId())) {
 					list.add(checkBlock);
 				}
 				
 				// Also check the next block out too.
 				checkBlock = checkBlock.getRelative(faces[x]);
-				if (canBreathe(checkBlock.getTypeId()) && canBreathe(checkBlock.getRelative(BlockFace.DOWN).getTypeId())){
+				if (canBreathe(checkBlock.getTypeId()) && canBreathe(checkBlock.getRelative(BlockFace.DOWN).getTypeId())) {
 					list.add(checkBlock);
 				}
 			}
@@ -165,7 +165,7 @@ public class NetherPortal {
 			}
 			
 			// Look above.
-			if(y2 <= 124) if(y2 != y1) {
+			if (y2 <= 124 && y2 != y1) {
 				checkBlock = world.getBlockAt(dest.getX(), y2, dest.getZ());
 				quality = checkPortalQuality(checkBlock);
 				
@@ -200,21 +200,21 @@ public class NetherPortal {
 		for (Block col : columns) {
 			// Stone platform, if needed.
 			checkBlock = world.getBlockAt(col.getX(), y - 1, col.getZ());
-			if(!canStand(checkBlock.getTypeId())){
-				if(world.getEnvironment() == Environment.NETHER) checkBlock.setType(Material.NETHERRACK);
+			if (!canStand(checkBlock.getTypeId())) {
+				if (world.getEnvironment() == Environment.NETHER) checkBlock.setType(Material.NETHERRACK);
 				else checkBlock.setType(Material.STONE);
 			}
 			
 			// Air pocket.
 			for (int yd = 0; yd < 3; ++yd) {
 				checkBlock = world.getBlockAt(col.getX(), y + yd, col.getZ());
-				if(!canBreathe(checkBlock.getTypeId())) checkBlock.setType(Material.AIR);
+				if (!canBreathe(checkBlock.getTypeId())) checkBlock.setType(Material.AIR);
 			}
 			
 			// Roof, if needed.
 			checkBlock = world.getBlockAt(col.getX(), y + 3, col.getZ());
-			if(canFall(checkBlock.getTypeId())){
-				if(world.getEnvironment() == Environment.NETHER) checkBlock.setType(Material.NETHERRACK);
+			if (canFall(checkBlock.getTypeId())) {
+				if (world.getEnvironment() == Environment.NETHER) checkBlock.setType(Material.NETHERRACK);
 				else checkBlock.setType(Material.STONE);
 			}
 		}
@@ -227,7 +227,7 @@ public class NetherPortal {
 				}
 				
 				// Be sure the portal is full of only air, at this point.
-				if((xd == 0 || xd == 1) && yd > -1 && yd < 3){
+				if ((xd == 0 || xd == 1) && yd > -1 && yd < 3) {
 					world.getBlockAt(x + xd, y + yd, z).setType(Material.AIR);
 				}
 			}
@@ -246,22 +246,22 @@ public class NetherPortal {
 		int quality = 0;
 		
 		// Check inside frame. Priority high-low, total 18.
-		if(canBreathe(checkBlock.getTypeId())) quality += 6;
-		if(canBreathe(checkBlock.getRelative(1, 0, 0).getTypeId())) quality += 6;
-		if(canBreathe(checkBlock.getRelative(0, 1, 0).getTypeId())) quality += 2;
-		if(canBreathe(checkBlock.getRelative(1, 1, 0).getTypeId())) quality += 2;
-		if(canBreathe(checkBlock.getRelative(0, 2, 0).getTypeId())) quality += 1;
-		if(canBreathe(checkBlock.getRelative(1, 2, 0).getTypeId())) quality += 1;
+		if (canBreathe(checkBlock.getTypeId())) quality += 6;
+		if (canBreathe(checkBlock.getRelative(1, 0, 0).getTypeId())) quality += 6;
+		if (canBreathe(checkBlock.getRelative(0, 1, 0).getTypeId())) quality += 2;
+		if (canBreathe(checkBlock.getRelative(1, 1, 0).getTypeId())) quality += 2;
+		if (canBreathe(checkBlock.getRelative(0, 2, 0).getTypeId())) quality += 1;
+		if (canBreathe(checkBlock.getRelative(1, 2, 0).getTypeId())) quality += 1;
 		
 		// Check ground under frame.  Priority mid, total 6.
-		if(canStand(checkBlock.getRelative(0, -1, 0).getTypeId())) quality += 3;
-		if(canStand(checkBlock.getRelative(1, -1, 0).getTypeId())) quality += 3;
+		if (canStand(checkBlock.getRelative(0, -1, 0).getTypeId())) quality += 3;
+		if (canStand(checkBlock.getRelative(1, -1, 0).getTypeId())) quality += 3;
 		
 		// Check ground around frame.  Priority low, total 4.
-		if(canStand(checkBlock.getRelative(0, -1, -1).getTypeId())) quality += 1;
-		if(canStand(checkBlock.getRelative(1, -1, -1).getTypeId())) quality += 1;
-		if(canStand(checkBlock.getRelative(0, -1, 1).getTypeId())) quality += 1;
-		if(canStand(checkBlock.getRelative(1, -1, 1).getTypeId())) quality += 1;
+		if (canStand(checkBlock.getRelative(0, -1, -1).getTypeId())) quality += 1;
+		if (canStand(checkBlock.getRelative(1, -1, -1).getTypeId())) quality += 1;
+		if (canStand(checkBlock.getRelative(0, -1, 1).getTypeId())) quality += 1;
+		if (canStand(checkBlock.getRelative(1, -1, 1).getTypeId())) quality += 1;
 		
 		return quality;
 	}
@@ -272,8 +272,8 @@ public class NetherPortal {
 		// and others you don't want to destroy too: 55,63,64,65,66,68,69,70,71,72,75,76,77,93,94
 		final int[] notSupporting = {0,6,8,9,10,11,37,38,39,40,50,51,59,83,85,90};
 		
-		for(int x = 0; x < notSupporting.length; ++x){
-			if(mat == notSupporting[x]) return false;
+		for (int x = 0; x < notSupporting.length; ++x) {
+			if (mat == notSupporting[x]) return false;
 		}
 		
 		return true;
@@ -285,7 +285,7 @@ public class NetherPortal {
 		final int[] isBreathable = {0,6,37,38,39,40,50,55,59,63,64,65,66,68,69,70,71,72,75,76,77,83,93,94};
 		
 		for (int x = 0; x < isBreathable.length; ++x) {
-			if(mat == isBreathable[x]) return true;
+			if (mat == isBreathable[x]) return true;
 		}
 		
 		return false;
@@ -297,7 +297,7 @@ public class NetherPortal {
 		final int[] isUnstable = {8,9,10,11,12,13};
 		
 		for (int x = 0; x < isUnstable.length; ++x) {
-			if(mat == isUnstable[x]) return true;
+			if (mat == isUnstable[x]) return true;
 		}
 		
 		return false;
